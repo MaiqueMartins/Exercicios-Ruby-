03ca904b4c2d9dfab2ad48cc50c5934df4dfdf48
Maique.Souza <mdsouzamartins@gmail.com>
2023-06-26 12:42:52 -0300

    Create a loop from 1 to 100 with while

---
+++ loop_while.rb
@@ -0,0 +1,35 @@
+numbers = 1
+
+while numbers >= 1
+  puts numbers
+  numbers = numbers + 1
+  if numbers == 101
+    break
+  end
+
+end
+
+
+
+
+
+
+
+# loop do numbers = numbers + 1
+
+#   puts numbers 
+
+#   if numbers == 100
+#     break
+#   end 
+# end
+
+
+
+
+# while numbers < 100 do
+#     number = numbers+1
+#   number + 1
+# end
+
+# puts number
