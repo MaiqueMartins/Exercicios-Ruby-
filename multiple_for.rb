a2452db85223b75a57d37acf294cb99af61e249d
Maique.Souza <mdsouzamartins@gmail.com>
2023-07-04 14:51:56 -0300

    Created multiple with for

---
+++ multiple_for.rb
@@ -0,0 +1,19 @@
+#  for number in 1..16
+#       result = number * 3
+#      puts result
+#  end 
+
+
+# for number in sequence
+#     sequence = {<=50}
+#       result = number * 3
+#      puts result
+#  end 
+
+
+ for number in 1..50 
+     if number % 3 == 0
+         puts number
+     end 
+    
+ end
